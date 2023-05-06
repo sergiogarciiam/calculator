2427c89e095808a7b1ce59bc704f944e1e4f6b01
Sergio Garcia <sergio.garciia.munoz@gmail.com>
2023-05-06 18:28:47 +0200

    Add keyboard events

--- scripts/script.js
+++ scripts/script.js
@@ -1,36 +1,58 @@
 const screen = document.querySelector(".screen");
 const buttons = document.querySelectorAll(".button");
+let startingPoint = true;
 
+document.addEventListener("keydown", addKeyboardEvents);
 buttons.forEach((button) => {
-  button.addEventListener("click", addToScreen);
+  button.addEventListener("click", () => {
+    addOperation(button);
+  });
 });
 
-function addToScreen(e) {
-  const screenText = createScreenText(e);
-  screen.appendChild(screenText);
-}
+function addKeyboardEvents(e) {
+  var key = e.key;
 
-function addToScreen(e) {
-  if (screen.hasChildNodes()) {
-    addOperation(e);
-  } else {
-    const screenText = document.createElement("p");
-    screenText.classList.add("screenText");
-    screenText.textContent = e.target.textContent;
-    screen.appendChild(screenText);
+  if (key === "Enter") {
+    key = "=";
+  } else if (key === "Backspace") {
+    key = "Back";
+  } else if (key === "Escape") {
+    key = "AC";
   }
+
+  buttons.forEach((button) => {
+    if (button.textContent === key) {
+      button.classList.add("button-active");
+
+      setTimeout(() => {
+        button.classList.remove("button-active");
+      }, 200);
+
+      addOperation(button);
+      return;
+    }
+  });
 }
 
-function addOperation(e) {
-  const screenText = document.querySelector(".screenText");
+function addOperation(button) {
+  let screenText = null;
+  if (startingPoint) {
+    screenText = document.createElement("p");
+    screenText.classList.add("screenText");
+    startingPoint = false;
+  } else {
+    screenText = document.querySelector(".screenText");
+  }
+
   let actualContent = screenText.textContent;
-  const newContent = e.target.textContent;
+  const newContent = button.textContent;
   let operation = false;
 
   switch (newContent) {
     case "AC":
       cleanScreen();
       operation = true;
+      startingPoint = true;
       break;
 
     case "Back":
